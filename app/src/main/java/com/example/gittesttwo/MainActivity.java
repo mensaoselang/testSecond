142067b195966b532b1bd2fb6f269b3b7d715f30
mensaoselang <2816775173@qq.com>
2019-03-27 14:00:29 +0800

    在分支上修改了内容2

--- app/src/main/java/com/example/gittesttwo/MainActivity.java
+++ app/src/main/java/com/example/gittesttwo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String str = "你好";
+        String strtwo="不好";
     }
 }
